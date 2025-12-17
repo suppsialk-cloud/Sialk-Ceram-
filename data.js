598fe44d45138cb7ac2dc0c306426f89730dbfad
suppsialk-cloud <suppsialk@gmail.com>
2025-12-17 13:02:00 +0330

    sialk ceram

---
+++ data.js
@@ -0,0 +1,480 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-room",
+      "name": "Living room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 0.01679786939416772,
+        "pitch": 0.05082111936258826,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0059313260243368,
+          "pitch": 0.23522428077491675,
+          "rotation": 0,
+          "target": "2-foyer"
+        },
+        {
+          "yaw": 1.7695630759404066,
+          "pitch": 0.15261906054079333,
+          "rotation": 0,
+          "target": "1-kitchen"
+        },
+        {
+          "yaw": 0.5530585834122412,
+          "pitch": 0.29358937096303706,
+          "rotation": 0,
+          "target": "5-bedroom"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.36392731927158195,
+          "pitch": 0.5903853016354059,
+          "title": "Velora",
+          "text": "80*80 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        },
+        {
+          "yaw": -2.921671726520117,
+          "pitch": -0.042205587333675965,
+          "title": "Nalina",
+          "text": "60*120 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        },
+        {
+          "yaw": 0.0750680942269053,
+          "pitch": 0.1730876229647187,
+          "title": "Shailin",
+          "text": "60*120 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.877938655748091,
+          "pitch": 0.24384912395046499,
+          "rotation": 0,
+          "target": "0-living-room"
+        },
+        {
+          "yaw": -2.0755049547647744,
+          "pitch": 0.2642965067001608,
+          "rotation": 0,
+          "target": "5-bedroom"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.20884265343831032,
+          "pitch": 0.33370241016137925,
+          "title": "Nalina",
+          "text": "60*120 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        },
+        {
+          "yaw": -1.4435541009129889,
+          "pitch": 0.8289695560501293,
+          "title": "Velora",
+          "text": "80*80 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        },
+        {
+          "yaw": -2.81554554109635,
+          "pitch": 0.6362833140429167,
+          "title": "Nalina",
+          "text": "60*120 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        }
+      ]
+    },
+    {
+      "id": "2-foyer",
+      "name": "Foyer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0688057391004815,
+          "pitch": 0.23207746405113383,
+          "rotation": 0,
+          "target": "0-living-room"
+        },
+        {
+          "yaw": 0.9647123730549492,
+          "pitch": 0.46405247573750863,
+          "rotation": 0,
+          "target": "6-labi"
+        },
+        {
+          "yaw": 0.1822627346839596,
+          "pitch": 0.34970471611148213,
+          "rotation": 0,
+          "target": "4-wc"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3305400129351845,
+          "pitch": 0.6134967359874128,
+          "title": "Shailin",
+          "text": "60*120 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        },
+        {
+          "yaw": -2.3538751535540925,
+          "pitch": 0.6204242993289331,
+          "title": "Velora",
+          "text": "80*80 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        }
+      ]
+    },
+    {
+      "id": "3-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0713508391324122,
+          "pitch": 0.4928199466374039,
+          "rotation": 0,
+          "target": "5-bedroom"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5663724564027532,
+          "pitch": -0.056141274006876074,
+          "title": "Sertina Light",
+          "text": "60*120 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        },
+        {
+          "yaw": -1.026865408624877,
+          "pitch": -0.23735203962192664,
+          "title": "Sertina Dark",
+          "text": "60*120 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        },
+        {
+          "yaw": 1.5243246583736534,
+          "pitch": 0.04511463278166339,
+          "title": "Puzzle&nbsp;Ceramic 01",
+          "text": "Area : 17&nbsp;cm²"
+        },
+        {
+          "yaw": 0.06639809268434504,
+          "pitch": 0.4757930099869725,
+          "title": "Washbasin 01",
+          "text": "100*47.5*16 cm<div>50 kg</div>"
+        },
+        {
+          "yaw": -0.8992048052196786,
+          "pitch": 0.8551222305166242,
+          "title": "Velora",
+          "text": "80*80 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        }
+      ]
+    },
+    {
+      "id": "4-wc",
+      "name": "WC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9776830085167667,
+          "pitch": 0.6033897031448099,
+          "rotation": 0,
+          "target": "2-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1832135772202168,
+          "pitch": 0.7015566495111152,
+          "title": "Washbasin 02",
+          "text": "100*50*17.5 cm<div>50 kg</div>"
+        },
+        {
+          "yaw": -0.11832768643243696,
+          "pitch": -0.05006565622528569,
+          "title": "Elmura",
+          "text": "60*120 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        },
+        {
+          "yaw": -2.3907977055251877,
+          "pitch": 1.0244684437387122,
+          "title": "Velora",
+          "text": "80*80 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        }
+      ]
+    },
+    {
+      "id": "5-bedroom",
+      "name": "Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6424692232126876,
+          "pitch": 0.36078970418017775,
+          "rotation": 0,
+          "target": "0-living-room"
+        },
+        {
+          "yaw": 0.8477707654334008,
+          "pitch": 0.2834027039813538,
+          "rotation": 0,
+          "target": "3-bathroom"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.44646803905575894,
+          "pitch": -0.03469155681937153,
+          "title": "Colorado",
+          "text": "60*120 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        },
+        {
+          "yaw": 0.9588234012289156,
+          "pitch": 0.67468482031917,
+          "title": "Velora",
+          "text": "80*80 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        },
+        {
+          "yaw": -0.44361488311759345,
+          "pitch": -0.034082664813466224,
+          "title": "Puzzle&nbsp;Ceramic 07",
+          "text": "Area : 15.5&nbsp;cm²"
+        }
+      ]
+    },
+    {
+      "id": "6-labi",
+      "name": "Labi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9883478548700095,
+          "pitch": 0.2961668948847276,
+          "rotation": 0,
+          "target": "2-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0939954609863562,
+          "pitch": 0.5132088403734318,
+          "title": "Ribera",
+          "text": "80*80 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        },
+        {
+          "yaw": 2.7301930134493837,
+          "pitch": 0.010565553863347787,
+          "title": "Rivaz",
+          "text": "60*120 cm<br><div><div>Matte</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        },
+        {
+          "yaw": 1.4424034359838398,
+          "pitch": 0.022498844593009437,
+          "title": "Black Rose",
+          "text": "60*120<br><div>Nano Polished</div><div>Random Faces</div>"
+        }
+      ]
+    }
+  ],
+  "name": "sialk ceram",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
